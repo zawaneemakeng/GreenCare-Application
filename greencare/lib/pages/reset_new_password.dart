b6aa13edb9cde658361254986f0a1d2ce1e6ef44
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-05-26 18:16:55 +0700

    Create reset_new_password.dart

---
+++ greencare/lib/pages/reset_new_password.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResetNewPassword extends StatefulWidget {
+  const ResetNewPassword({super.key});
+
+  @override
+  State<ResetNewPassword> createState() => _ResetNewPasswordState();
+}
+
+class _ResetNewPasswordState extends State<ResetNewPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
