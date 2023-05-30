f9090282c7d914744320b0ae1729517172105845
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-05-30 15:27:37 +0700

    Create serializers.py

---
+++ Django Web Server/myapp/usermanage/serializers.py
@@ -0,0 +1,8 @@
+# from rest_framework import serializers
+# from .models import ResetPasswordToken
+
+# class ResetSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ResetPasswordToken
+#         fields = ('__all__')#'__all__' เอาทั้งหมดapi
+    
