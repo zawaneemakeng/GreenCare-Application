e294edb9b29e7ab2ee6b07d6ec010da2ac4932ee
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-06-01 19:34:24 +0700

    Update models.py

--- Django Web Server/myapp/usermanage/models.py
+++ Django Web Server/myapp/usermanage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .emailsystem import sendthai
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE) #user.profile.token
@@ -7,6 +8,14 @@ class Profile(models.Model):
     mobile = models.CharField(max_length=20,null=True, blank=True) #ไม่บังตับใส่
     token = models.CharField(max_length=100,default='-')
     verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+class ResetPasswordToken(models.Model):
+    # ดึงอีกโมเดล   ondelete=models.CASCADE =ถ้าuserถูกลบ จะลบด้วย
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=100)
 
     def __str__(self):
         return self.user.username
