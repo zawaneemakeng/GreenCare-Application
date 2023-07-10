a08101fa82c2d0a44d7799ac88c252f288a785eb
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-07-10 21:26:25 +0700

    Update shop.dart

--- greencare/lib/pages/shop/shop.dart
+++ greencare/lib/pages/shop/shop.dart
@@ -1,14 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get_connect/http/src/request/request.dart';
-import 'package:path/path.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:greencare/utils/api_url.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -19,8 +16,101 @@ class ShopPage extends StatefulWidget {
 
 class _ShopPageState extends State<ShopPage> {
   File? selectedImage;
-  String message = "";
-  File? _image;
+  List imageList = [];
+  String fileName = "";
+  String getlast_img = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getImage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(children: [
+            imageProfile(),
+            SizedBox(
+              height: 40,
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Future getImage() async {
+    var url = Uri.http('${urlH()}', '/media/profile/');
+    var response = await http.get(url);
+    var result = utf8.decode(response.bodyBytes);
+    setState(() {
+      imageList = json.decode(result);
+      print(imageList);
+      final reversedIndex = imageList.length - 1;
+      getlast_img = imageList[reversedIndex]['cover_img'];
+      print(getlast_img);
+    });
+  }
+
+  Widget imageProfile() {
+    return Center(
+      child: Stack(children: <Widget>[
+        SizedBox(
+          height: 40,
+        ),
+        CachedNetworkImage(
+          imageUrl: '"http://${urlH()}${getlast_img}',
+          placeholder: (context, url) => const CircularProgressIndicator(),
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
+            size: 100,
+            color: Colors.red,
+          ),
+          imageBuilder: (context, imageProvider) => Container(
+            width: 130,
+            height: 130,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1),
+                shape: BoxShape.circle,
+                image: DecorationImage(image: imageProvider, fit: BoxFit.fill)),
+          ),
+        ),
+        Positioned(
+            bottom: 15.0,
+            right: 20.0,
+            child: InkWell(
+              onTap: () {
+                showBottomSheet(
+                    context: this.context, builder: (builder) => bottomSheet());
+                // enableDrag: false);
+              },
+              child: Icon(
+                Icons.camera_alt,
+                color: Colors.grey[600],
+                size: 26,
+              ),
+            )),
+        SizedBox(
+          height: 100,
+        )
+      ]),
+    );
+  }
+
+  Future _getImage(ImageSource source) async {
+    final image = await ImagePicker().pickImage(source: source);
+    selectedImage = File(image!.path);
+    setState(() {
+      print(selectedImage);
+      uploadImage();
+      Navigator.pop(this.context);
+    });
+  }
+
   Future uploadImage() async {
     final request =
         http.MultipartRequest("POST", Uri.parse("http://${urlH()}/api/image"));
@@ -31,148 +121,42 @@ class _ShopPageState extends State<ShopPage> {
     request.headers.addAll(headers);
     final response = await request.send();
     http.Response res = await http.Response.fromStream(response);
-  }
-
-  Future _getImage(ImageSource source) async {
-    final image = await ImagePicker().pickImage(source: source);
-    selectedImage = File(image!.path);
     setState(() {
-      print(selectedImage);
+      fileName = selectedImage!.path.split("/").last;
+      print("Name: ${fileName}");
+      getlast_img = "/media/profile/${fileName}";
     });
   }
 
-  // Future<File> saveFilePermanently(String imagePath) async {
-  //   final directory = await getApplicationDocumentsDirectory();
-  //   final name = basename(imagePath);
-  //   final image = File('${directory.path}/$name');
-  //   print(name);
-
-  //   setState(() {
-  //     _image = image;
-  //     print(_image);
-  //     uploadImage();
-  //   });
-  //   return File(imagePath).copy(image.path);
-  // }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(children: <Widget>[
-            SizedBox(
-              height: 40,
-            ),
-            selectedImage == null
-                ? Image.network(
-                    "https://cdn-icons-png.flaticon.com/512/3607/3607444.png",
-                    width: 200,
-                    height: 200,
-                  )
-                : Image.file(
-                    selectedImage!,
-                    width: 200,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-            SizedBox(
-              height: 40,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  _getImage(ImageSource.gallery);
-                },
-                child: Text('fkjdskf')),
-            SizedBox(
-              height: 40,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  _getImage(ImageSource.camera);
-                },
-                child: Text('camara')),
-            SizedBox(
-              height: 40,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  uploadImage();
-                },
-                child: Text('Upload')),
-          ]),
-        ),
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(this.context).size.width,
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+        children: <Widget>[
+          Text("fdkfldfk"),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: <Widget>[
+              TextButton.icon(
+                  onPressed: () {
+                    _getImage(ImageSource.camera);
+                  },
+                  icon: Icon(Icons.camera_alt),
+                  label: Text("camera")),
+              TextButton.icon(
+                  onPressed: () {
+                    _getImage(ImageSource.gallery);
+                  },
+                  icon: Icon(Icons.browse_gallery),
+                  label: Text("gallery")),
+            ],
+          )
+        ],
       ),
     );
   }
-
-  // Widget imageProfile() {
-  //   return Center(
-  //     child: Stack(children: <Widget>[
-  //       CircleAvatar(
-  //           radius: 80,
-  //           backgroundImage: NetworkImage(
-  //               "https://cdn-icons-png.flaticon.com/512/3607/3607444.png")),
-  //       Positioned(
-  //           bottom: 20,
-  //           right: 25,
-  //           child: InkWell(
-  //               onTap: () {
-  //                 showModalBottomSheet(
-  //                   context: context,
-  //                   builder: ((builder) => bottomSheet()),
-  //                   backgroundColor: Colors.white,
-  //                 );
-  //               },
-  //               child: Icon(Icons.camera_alt, color: Colors.grey, size: 26.0)))
-  //     ]),
-  //   );
-  // }
-
-  // Widget bottomSheet() {
-  //   return Container(
-  //     height: 80,
-  //     margin: EdgeInsets.only(right: 10, top: 20, bottom: 10),
-  //     child: Column(
-  //       children: [
-  //         Text("เลือกรูปภาพ"),
-  //         SizedBox(
-  //           height: 10.0,
-  //         ),
-  //         Row(
-  //           mainAxisAlignment: MainAxisAlignment.center,
-  //           children: <Widget>[
-  //             TextButton.icon(
-  //                 onPressed: () {
-  //                   _getImage(ImageSource.camera);
-  //                 },
-  //                 icon: Icon(
-  //                   Icons.camera_alt,
-  //                   color: Colors.green,
-  //                 ),
-  //                 label: Text(
-  //                   "Camera",
-  //                   style: TextStyle(color: Colors.black),
-  //                 )),
-  //             TextButton.icon(
-  //                 onPressed: () {
-  //                   _getImage(ImageSource.gallery);
-  //                 },
-  //                 icon: Icon(Icons.image, color: Colors.green),
-  //                 label: Text(
-  //                   "Gallery",
-  //                   style: TextStyle(color: Colors.black),
-  //                 )),
-  //           ],
-  //         )
-  //       ],
-  //     ),
-  //   );
-  // }
 }
