28413cd1a616b803867572a1d857a120bae13f22
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-07-17 21:30:06 +0700

    Update plant.dart

--- greencare/lib/pages/plant/plant.dart
+++ greencare/lib/pages/plant/plant.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:greencare/pages/plant/add_plants.dart';
+import 'package:greencare/pages/plant/show_my_plant.dart';
+import 'package:greencare/pages/plant/update_plant.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:get/get.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:greencare/utils/api_url.dart';
 
 class PlantPage extends StatefulWidget {
   const PlantPage({super.key});
@@ -15,7 +17,7 @@ class PlantPage extends StatefulWidget {
 class _PlantPageState extends State<PlantPage> {
   List plantsItems = [];
   int? userID;
-  bool isButtonEnabled = true;
+  bool isButtonEnabled = false;
   @override
   void initState() {
     super.initState();
@@ -35,7 +37,8 @@ class _PlantPageState extends State<PlantPage> {
               child: FloatingActionButton(
                 onPressed: () async {
                   final reversedIndex = plantsItems.length - 1;
-                  if (plantsItems[reversedIndex]['status'] == true) {
+                  if (plantsItems.isEmpty ||
+                      plantsItems[reversedIndex]['status'] == true) {
                     String refresh = await Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => AddPlants()),
@@ -45,17 +48,16 @@ class _PlantPageState extends State<PlantPage> {
                     }
                   } else {
                     final snackBar = SnackBar(
-                      content: const Text('Yay! A SnackBar!'),
+                      content: const Text(
+                          'ไม่สามารถเพิ่มได้ กรุณาเปลี่ยนสถานะเก็บเกี่ยว!'),
                       action: SnackBarAction(
-                        label: 'Undo',
+                        label: 'รับทราบ',
                         onPressed: () {
                           // Some code to undo the change.
                         },
                       ),
                     );
 
-                    // Find the ScaffoldMessenger in the widget tree
-                    // and use it to show a SnackBar.
                     ScaffoldMessenger.of(context).showSnackBar(snackBar);
                   }
                 },
@@ -75,13 +77,52 @@ class _PlantPageState extends State<PlantPage> {
           children: [
             Padding(
               padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-              child: Text('ค่าใช้จ่ายล่าสุด',
+              child: Text('ฟาร์มของคุณ',
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
             ),
+            ShowPlant(),
             Plants(),
-            SizedBox(
-              height: 20,
-            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget noPlant() {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 15,
+        ),
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            color: Colors.grey[300], borderRadius: BorderRadius.circular(8.0)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.qr_code,
+                  color: Colors.green,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "กรุณาเพิ่มข้อมูลการปลูกผัก",
+                      style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -91,53 +132,88 @@ class _PlantPageState extends State<PlantPage> {
   Widget Plants() {
     return Expanded(
       child: ListView.builder(
-        itemCount: plantsItems.length.clamp(0, 6),
-        shrinkWrap: false,
-        itemBuilder: (context, int index) {
-          final reversedIndex = plantsItems.length - 1 - index;
-          return Container(
-            margin: EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 15,
-            ),
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(8.0)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.arrow_circle_down_outlined,
-                      color: Colors.green,
+          itemCount: plantsItems.length.clamp(0, 6),
+          shrinkWrap: false,
+          itemBuilder: (context, int index) {
+            final reversedIndex = plantsItems.length - 1 - index;
+            plantsItems.isNotEmpty
+                ? Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      top: 15,
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      // crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("${plantsItems[reversedIndex]['plantstype']}"),
-                        Text(
-                          "${plantsItems[reversedIndex]['detail']}",
-                          style:
-                              TextStyle(color: Colors.grey[600], fontSize: 12),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.arrow_circle_down_outlined,
+                              color: Colors.green,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    "${plantsItems[reversedIndex]['plantstype']}"),
+                                Text(
+                                  "${plantsItems[reversedIndex]['detail']}",
+                                  style: TextStyle(
+                                      color: Colors.grey[600], fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Text("+ ${plantsItems[reversedIndex]['detail']}",
+                            style: TextStyle(fontSize: 14)),
+                      ],
+                    ),
+                  )
+                : Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                      top: 15,
+                    ),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.arrow_circle_down_outlined,
+                              color: Colors.green,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("กรุณาเพิ่มข้อมูลการปลูกผัก"),
+                              ],
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-                Text("+ ${plantsItems[reversedIndex]['detail']}",
-                    style: TextStyle(fontSize: 14)),
-              ],
-            ),
-          );
-        },
-      ),
+                  );
+          }),
     );
   }
 
@@ -160,14 +236,16 @@ class _PlantPageState extends State<PlantPage> {
   }
 
   Future getPlants() async {
-    var url = Uri.http('00000:8000', '/api/plants/$userID');
+    var url = Uri.http(urlH(), '/api/plants/$userID');
     var response = await http.get(url);
     // var result = json.decode(response.body);
     var result = utf8.decode(response.bodyBytes);
     setState(() {
       plantsItems = json.decode(result);
-      final reversedIndex = plantsItems.length - 1;
-      if (plantsItems[reversedIndex]['stutus'] == false) {}
+      print(plantsItems);
+      print(plantsItems.length);
+      // final reversedIndex = plantsItems.length - 1;
+      // if (plantsItems[reversedIndex]['stutus'] == false) {}
       // for (int i = 0; i < incomeItem.length; i++) {
       //   if (incomeItem[i]['transtype'] == 'Income') {
       //     totalIncome += double.parse(incomeItem[i]['amount']);
