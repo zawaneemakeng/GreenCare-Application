b22b91e9560e48d60b98ba7c739778ffb621586d
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-08-10 23:13:34 +0700

    Create custom.dart

---
+++ greencare/lib/utils/custom.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const dividerline = Color(0xffE4E4EE);
+  static const cardColor = Color(0xffE9ECF1);
+  static const textColorBlack = Color(0xff171717);
+  static const firstGradientColor = Color(0xff408ADE);
+  static const secondGradientColor = Color(0xff5286CD);
+}
+
+class ButtonColor {
+  static const bgcolor = Color(0xff3AAA94);
+}
+
+class SnackBarColor {
+  static const bgcolor = Color(0xff757575);
+}
+
+class FontCustom {
+  static const size = TextStyle(fontSize: 12);
+
+  static const bgcolor = Color(0xff3AAA94);
+}
+
+class IconColor {
+  static const color = Color(0xff858585);
+}
