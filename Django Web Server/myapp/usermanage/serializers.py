c38710c3d08286c42761e93ae9b3ca132f6d30b5
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-08-20 20:39:51 +0700

    Update serializers.py

--- Django Web Server/myapp/usermanage/serializers.py
+++ Django Web Server/myapp/usermanage/serializers.py
@@ -1,8 +1,36 @@
-# from rest_framework import serializers
-# from .models import ResetPasswordToken
+from rest_framework import serializers
+from .models import *
 
-# class ResetSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = ResetPasswordToken
-#         fields = ('__all__')#'__all__' เอาทั้งหมดapi
-    
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'#'__all__' เอาทั้งหมดapi
+        
+
+class OTPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResetPasswordToken
+        fields = '__all__'#'__all__' เอาทั้งหมดapi
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ('id','user','amount','detail','transtype','date')
+
+
+class PlantsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlantManager
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+
