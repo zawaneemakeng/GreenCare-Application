3dbeebb8fc8e0e05baad7cd76afe518483072562
zawanee <89334887+zawaneemakeng@users.noreply.github.com>
2023-10-27 21:21:08 +0700

    Create styles.dart

---
+++ greencare/lib/utils/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kTitleStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 26.0,
+  height: 1.5,
+);
+
+const kSubtitleStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 18.0,
+  height: 1.2,
+);
